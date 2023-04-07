923708e06a877fa0842b3e9fac65c5ed6caa2a91
paulovictormourao <mouraopaulovictor@gmail.com>
2023-04-07 14:05:31 -0400

    New exercise and add commentary to Hello World

---
+++ Java/Java How To Program/src/Addition.java
@@ -0,0 +1,24 @@
+/* Addition Of Two Terms With Scanner
+Author: Paulo Victor Mourão
+Last Modified on: 29/03/2023
+ */
+
+import java.util.Scanner;
+
+public class Addition {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
+        int x1, x2, y;
+
+        System.out.print("Type an int: ");
+        x1 = input.nextInt();
+
+        System.out.print("Type another: ");
+        x2 = input.nextInt();
+
+        y = x1 + x2;
+
+        System.out.printf("The result is: %d", y);
+    }
+}
--- Java/Java How To Program/src/HelloWorld.java
+++ Java/Java How To Program/src/HelloWorld.java
@@ -1,5 +1,11 @@
+/*Hello World!
+Author: Paulo Victor Mourão
+Last Modified: 29/03/2023
+ */
+
 public class HelloWorld {
     public static void main(String[] args ){
+
         System.out.println("Hello World!");
     }
 }
