21f8a7d2871174a2362c2b53a93e5a525b01312e
paulovictormourao <mouraopaulovictor@gmail.com>
2023-06-09 11:59:13 -0400

    time test

--- Introduction to Algorithms (Cormen's book using C)/bubble.c
+++ Introduction to Algorithms (Cormen's book using C)/bubble.c
@@ -1,30 +1,48 @@
+#include <time.h>
 #include <stdio.h>
 
-int main ()
-{
-    int i, j, aux, swap = 1;
-    int list[] = {5, 2, 4, 1, 3};
 
-    for (i = 0; (i < 5) && (swap == 1); i++)
+
+int main (int argc, char** argv)
+{
+    int i, j, aux, swap = 1, count = 1;
+    int size = 100;
+    int list[size];
+    double start, finish, elapsed;
+
+    start = (double ) clock() / CLOCKS_PER_SEC;
+
+    for (i = 0; i < size; i++){
+        scanf("%d", &list[i]);
+    }
+
+
+    for (i = 0; (i < size) && (swap == 1); i++)
     {
         swap = 0;
 
-        for( j = 0; j < 5 - i - 1; j++ ) {
+        for( j = 0; j < size - i - 1; j++ ) {
+
             if (list[j] > list[j + 1]) {
                 swap = 1;
                 aux = list[j];
                 list[j] = list[j + 1];
                 list[j + 1] = aux;
             }
-
+                count++;
         }
     }
 
 
-    for (i = 0; i < 5; i++)
+    /*for (i = 0; i < size; i++)
     {
         printf("%d ", list[i]);
-    }
+    }*/
+
+    finish = (double ) clock() / CLOCKS_PER_SEC;
+    elapsed = (double ) finish - start;
+
+    printf("\n %f s\n", elapsed);
 
     return 0;
 }
